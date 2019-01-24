364b99f21e3ff3f6b9aa215b9834950f7403863b
kieran <kierandaly88@gmail.com>
2019-01-24 09:55:47 +0000

    Initial Commit

---
+++ kieran/dummyapp/app/views.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app
+@app.route('/')
+def index():
+ returnDict = {}
+ returnDict['user'] = 'COMP30670' # Feel free to put your name here!
+ returnDict['title'] = 'Home'
+ return render_template("index.html", **returnDict)
